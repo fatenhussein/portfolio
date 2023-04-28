a69dcb356d34fd39e24629c67d499b3eec5788df
faten <faten.a.alhussein@gmail.com>
2023-04-28 22:41:28 +0300

    sec

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 const burger_manu = document.getElementById("burger");
 const close = document.getElementById("close");
 const nav = document.querySelector(".nav-links");
-let x = window.matchMedia("(max-width: 768px)");
+let x = window.matchMedia("()");
+
+let size =  window.matchMedia( "(min-width: 1000px)");
+
+
+function media (size){
+  if(size.match){
+burger_manu.style.display="none";
+close.style.display = "none";
+
+  }
+
+}
+
+
 const opennavmenu = () => {
   nav.classList.add("nav-active");
   close.style.display = "block";
   burger_manu.style.display = "none";
+  
 };
 
 const closenav = () => {
   nav.classList.remove("nav-active");
   burger_manu.style.display = "block";
   close.style.display = "none";
-  setTimeout(() => {
-    document.location.reload();
-  }, 3000);
+ 
 };
-
+ 
 burger_manu.addEventListener("click", opennavmenu);
 
 close.addEventListener("click", closenav);
-
+window.onresize= media(size);
